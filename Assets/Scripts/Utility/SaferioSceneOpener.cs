8f6de46353bc01078da2a367b00a233e94a897ca
reed-jim <nocoop527@gmail.com>
2024-11-04 21:43:39 +0700

    scene opener tool

---
+++ Assets/Scripts/Utility/SaferioSceneOpener.cs
@@ -0,0 +1,18 @@
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class SaferioSceneOpener : EditorWindow
+{
+    [MenuItem("Tools/Open Scene/Menu")]
+    public static void OpenMenuScene()
+    {
+        EditorSceneManager.OpenScene("Assets/Scenes/Menu.unity");
+    }
+
+    [MenuItem("Tools/Open Scene/Gameplay")]
+    public static void OpenGameplayScene()
+    {
+        EditorSceneManager.OpenScene("Assets/Scenes/Gameplay.unity");
+    }
+}
